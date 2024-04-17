0b87c8985dc743cd9f48ddb1d5483310311c7eb5
Anteneh <anttesdev@gmail.com>
2024-04-17 16:28:30 -0700

    do create

--- models/base_model.py
+++ models/base_model.py
@@ -15,12 +15,10 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            self.id = kwargs.get('id', str(uuid.uuid4()))
+            self.created_at = kwargs.get('created_at', datetime.now())
+            self.updated_at = kwargs.get('updated_at', datetime.now())
+
 
     def __str__(self):
         """Returns a string representation of the instance"""
